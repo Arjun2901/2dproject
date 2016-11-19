08e518673bb992ac7ce8fc986c45b331e9e8e460
bhavyar16 <bhavyar16@gmail.com>
2016-11-19 17:31:47 -0500

    collision

--- tracker.start/manager.d
+++
@@ -1,4 +0,0 @@
-manager.o manager.d : manager.cpp multisprite.h drawable.h vector2f.h frame.h \
- twowayMsprite.h sprite.h gamedata.h parseXML.h manager.h ioManager.h \
- clock.h world.h viewport.h player.h collisionStrategy.h Hud.h \
- scaledSprite.h aaline.h health.h
--- tracker.start/twowayMsprite.d
+++
@@ -1,3 +0,0 @@
-twowayMsprite.o twowayMsprite.d : twowayMsprite.cpp twowayMsprite.h drawable.h vector2f.h \
- frame.h gamedata.h parseXML.h frameFactory.h sprite.h explodingSprite.h \
- chunk.h
